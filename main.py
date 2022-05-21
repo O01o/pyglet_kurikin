d5b34c5ede6a79e724e9612ffc36bfb5038fd2a7
AyatoMatsui <77559448+O01o@users.noreply.github.com>
2022-05-21 23:53:36 +0900

    途中まで

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from cv2 import polylines
 import pyglet
 import random
 from pyglet import resource
+from pyglet.window import mouse
+import math
 
+# ウィンドウの描画
 window = pyglet.window.Window(640, 480, "KurikinTest")
 
+# キンの画像ロード
 player_kins_image = pyglet.image.load("images/a0.png")
 enemy_kins_image = pyglet.image.load("images/b0.png")
 error_image = pyglet.image.load("images/a1.png")
 
+# 自分または相手のキンの描画
 def kins_maker(num_kins, batch=None, img=error_image, x0=0, y0=0, x1=640, y1=480):
     kins = []
     for i in range(num_kins):
@@ -23,24 +28,18 @@ def kins_maker(num_kins, batch=None, img=error_image, x0=0, y0=0, x1=640, y1=480
     return kins
 
 # タッチペンで描く円(曲線)を描画するためのプログラム
-'''
-nodes = [[0,0],[50,50],[20,80]]
-edges = []
-for node_num in range(nodes)-1:
-    edge = [nodes[node_num][0],nodes[node_num][1], nodes[node_num+1][0]-nodes[node_num+1][1]]
-    edges.append = edge
-    
-def polyline_maker(edges, batch=None):
-    polyline = []
-    for edge in edges:
-        element = pyglet.shapes.Line(edge[0],edge[1],edge[2],edge[3],width=3,batch=batch)
-        polyline.append(element)
-    return polyline
-'''
+def circle_node_add(x,y,tp=(0,0)):
+    edge = pyglet.shapes.Line(x,y,tp[0],tp[1],width=5)
+    return edge
 
+# バッチの用意, 自分のキンと相手のキンを描画
 main_batch = pyglet.graphics.Batch()
+circle_batch = pyglet.graphics.Batch()
 kins_p = kins_maker(num_kins=10, batch=main_batch, img=player_kins_image, x0=0, y0=240, x1=240, y1=480) # 自分のキンの描画
-kins_e = kins_maker(num_kins=10, batch=main_batch, img=enemy_kins_image, x0=480, y0=0, x1=640, y1=240) # 自分のキンの描画
+kins_e = kins_maker(num_kins=10, batch=main_batch, img=enemy_kins_image, x0=400, y0=0, x1=640, y1=240) # 自分のキンの描画
+circle_point_tmp = 0,0
+circle_polygon = []
+kins_state = False
 # polyline = polyline_maker(edges, batch=main_batch) # 多角形描画
 
 @window.event
@@ -48,8 +47,42 @@ def on_draw():
     window.clear()
     main_batch.draw() # kins_pのオブジェクトリストもバッチで一括描画！
 
-def update():
+@window.event
+def on_mouse_press(x,y,button,modifiers):
+    global circle_point_tmp
+    global circle_polygon
+    circle_point_tmp = x,y
+    circle_polygon.append(circle_point_tmp)
+    print(circle_point_tmp,"pressed")
+    
+@window.event
+def on_mouse_drag(x,y,dx,dy,buttons,modifiers):
+    global circle_point_tmp
+    global circle_polygon
+    min_distance = 20
+    distance = math.sqrt((x-circle_point_tmp[0])**2 + (y-circle_point_tmp[1])**2)
+    if distance >= min_distance:
+        edge = circle_node_add(x,y,tp=circle_point_tmp)
+        circle_point_tmp = x,y
+        circle_polygon.append(circle_point_tmp)
+        print(circle_point_tmp,"drag")
+        # edge.draw()
+    
+@window.event
+def on_mouse_release(x,y,button, modifiers):
+    global circle_point_tmp
+    global circle_polygon
+    circle_point_tmp = x,y
+    circle_polygon.append(circle_point_tmp)
+    circle_polygon.append(circle_polygon[0])
+    print(circle_point_tmp,"released")
+    print("polygon", circle_polygon)
+    circle_polygon = []
+
+'''
+def callback(dt):
     pass
+'''
 
 pyglet.app.run()
-pyglet.clock.schedule_interval(update, 1/30)
+# pyglet.clock.schedule_interval(callback, 1/30)
---
+++ tmp.py
@@ -0,0 +1,7 @@
+hoge = 0,0
+fuga = 2,5
+print(type(hoge))
+print(type(hoge[0]))
+print(hoge == fuga)
+if not hoge == fuga:
+    print(hoge -- fuga)
